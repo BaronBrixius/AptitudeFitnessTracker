eb9ba7b773540b5732ee14ceb210ec6a859da019
Max Anderson <ma225bj@student.lnu.se>
2021-05-22 20:18:51 +0200

    In progress data mapper for Routine -> RoutineEntity

--- app/src/main/java/com/example/aptitudefitnesstracker/application/Routine.kt
+++ app/src/main/java/com/example/aptitudefitnesstracker/application/Routine.kt
@@ -1,14 +1,19 @@
 package com.example.aptitudefitnesstracker.application
 
 class Routine { //not sure this will end up being needed
-    var id:Int? = null
-    var name: String? = null
+    var id:Int = 0
+    var name: String = ""
     var tags:List<String>? = null
 
     // Default constructor required for calls to
     // DataSnapshot.getValue(User.class)
     constructor() {}
     constructor(name: String?) {
-        this.name = name
+        if (name != null) {
+            this.name = name
+        }
     }
+
+    constructor(id: Int, name: String)
+    constructor(id: Int, name: String, tags: List<String>)
 }
